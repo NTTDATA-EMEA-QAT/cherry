370b4f1c810833c5b922976a1dcc35da34ebd9e3
mamalisk <mamalis.kostas@gmail.com>
2016-04-14 14:16:32 +0100

    fixing memory issue

--- src/main/java/io/magentys/CoreMemory.java
+++ src/main/java/io/magentys/CoreMemory.java
@@ -2,6 +2,7 @@ package io.magentys;
 
 import io.magentys.exceptions.NotAvailableException;
 import io.magentys.utils.Any;
+import io.magentys.utils.Clazz;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -26,7 +27,7 @@ public class CoreMemory implements Memory<String> {
         Any result = map.get(key);
         if(result == null) return null;
         final Object unwrapped = result.get();
-        if (unwrapped.getClass() == clazz) {
+        if (Clazz.isClassOrSubclass(clazz, unwrapped.getClass())) {
            return (VALUE) unwrapped;
         }
         throw new NotAvailableException("Expected value in memory was not of type: " + clazz);
