e2d1f4ad90f3ef921c655a170a108a7ea88ce6f4
mamalisk <mamalis.kostas@gmail.com>
2016-04-14 14:06:54 +0100

    fixing issue with lists in memorable

--- src/main/java/io/magentys/utils/Clazz.java
+++ src/main/java/io/magentys/utils/Clazz.java
@@ -1,10 +1,24 @@
 package io.magentys.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Clazz {
 
     public static <OBJECT> boolean isClassOrSubclass(Class<OBJECT> clazz, Class<?> classOfAny) {
-        return classOfAny.equals(clazz) ||
-                classOfAny.getSuperclass().equals(clazz) ||
-                clazz.isAssignableFrom(classOfAny);
+        return  classOfAny.equals(clazz) ||
+                clazz.isAssignableFrom(classOfAny) ||
+                fromSuperClass(clazz, classOfAny) || extendedFrom(clazz, classOfAny);
     }
+
+    private static <OBJECT> boolean fromSuperClass(Class<OBJECT> clazz, Class<?> classOfAny) {
+        if(classOfAny.getSuperclass() == null) return false;
+        return classOfAny.getSuperclass().equals(clazz);
+    }
+
+    private static <OBJECT> boolean extendedFrom(Class<OBJECT> clazz, Class<?> classOfAny){
+        if(clazz.getSuperclass() == null) return false;
+        return clazz.getSuperclass().equals(classOfAny);
+    }
+
 }
