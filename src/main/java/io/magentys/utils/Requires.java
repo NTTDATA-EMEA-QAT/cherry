215b860cbd4e376d3c8000f959629e5b65027868
Slawek Puklo <piszdomniejeszcze@gmail.com>
2016-04-18 12:09:25 +0100

    Adding requiresNotNull, a null check syntax sugar method

--- src/main/java/io/magentys/utils/Requires.java
+++ src/main/java/io/magentys/utils/Requires.java
@@ -4,7 +4,17 @@ import io.magentys.exceptions.RequirementException;
 
 public class Requires {
 
-    public static void requires(Boolean condition, String errorMessage){
-        if(!condition) throw new RequirementException(errorMessage);
+    public static void requires(final Boolean condition, final String errorMessage) {
+        if (!condition) {
+            throw new RequirementException(errorMessage);
+        }
     }
+
+    public static <T> T requiresNotNull(final T value, final String errorMessage) {
+        if (value == null) {
+            throw new RequirementException(errorMessage);
+        }
+        return value;
+    }
+
 }
