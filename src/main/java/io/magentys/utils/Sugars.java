dcea333d95121b0b3fa3a58164b5b75686d88974
mamalisk <mamalis.kostas@gmail.com>
2016-05-19 11:31:32 +0100

    additional sugars

--- src/main/java/io/magentys/utils/Sugars.java
+++ src/main/java/io/magentys/utils/Sugars.java
@@ -6,7 +6,35 @@ public class Sugars {
         return item;
     }
 
-    public static <ITEM> ITEM but(final ITEM item){
+    public static <ITEM> ITEM but(final ITEM item) {
         return item;
     }
+
+    public static <ITEM> ITEM given(final ITEM item){
+        return item;
+    }
+    public static <ITEM> ITEM when(final ITEM item){
+        return item;
+    }
+    public static <ITEM> ITEM then(final ITEM item){
+        return item;
+    }
+    public static <ITEM> ITEM although(final ITEM item){
+        return item;
+    }
+
+    public static <ITEM> ITEM meanwhile(final ITEM item){
+        return item;
+    }
+
+    public static <ITEM> ITEM with(final ITEM item){
+        return item;
+    }
+
+    public static <ITEM> ITEM without(final ITEM item){
+        return item;
+    }
+
+
+
 }
